7ec05eb4a2db89cb35811b1592f91a512f97108d
appalanaidu <appalanaidu87128@gmail.com>
2025-06-20 18:15:20 +0530

    print function

---
+++ JavaPrograms/src/main/java/JavaPrograms/PrintMethod.java
@@ -0,0 +1,16 @@
+package JavaPrograms;
+
+public class PrintMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//println - will print in the next line
+		System.out.println("welcome");
+		System.out.println("java");
+		System.out.println(10);
+		System.out.print("welcome ");
+		System.out.print("to java");
+
+	}
+
+}
